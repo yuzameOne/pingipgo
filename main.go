776a53e05327002628ba3a41117ba2682b567ab2
yuzameOne <E.homayko@yandex.ru>
2020-11-18 16:18:18 +1000

    read all file

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -9,6 +10,14 @@ import (
 func main() {
 
 	fmt.Println("start server")
+
+	ip, err := ioutil.ReadFile("iplist.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("\nFile contents: \n%s", ip)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
