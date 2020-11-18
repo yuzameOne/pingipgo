a3c2d4a40afd7f946e93531d8dfc6bc8663268cd
yuzameOne <E.homayko@yandex.ru>
2020-11-18 23:21:04 +1000

    simple ping ip address (rewrite)

--- main.go
+++ main.go
@@ -10,9 +10,6 @@ import (
 	"strings"
 )
 
-var ipLost string = "4.25.115.81"
-var ipReceived string = "87.250.250.242"
-
 func ping(ip ...string) {
 
 	// c - sending count ECHO_REQUEST packets
@@ -20,15 +17,16 @@ func ping(ip ...string) {
 	// Only super-user may set interval to values less than 0.2 seconds
 	// w - deadline Specify a timeout, in seconds, before ping exits regardless of how
 	// many packets have been sent or received.
-	fmt.Printf("ip array : %s", ip[1])
-	out, _ := exec.Command("ping", ipReceived, "-c 2", "-i 1", "-w 2").Output()
+	fmt.Printf("ip array : %s", ip[3]) //dedug out
+	
+	out, _ := exec.Command("ping", ip[3], "-c 2", "-i 1", "-w 2").Output()
 
-	fmt.Println(string(out)) //dedug out
+	fmt.Println(string(string(out))) //dedug out
 
-	if strings.Contains(string(out), "100% packet loss") {
-		fmt.Println("Dead mother fucker")
-	} else {
+	if strings.Contains(string(out), "2 received") {
 		fmt.Println("Yeap, I'am ALIVEEE")
+	} else {
+		fmt.Println("Dead Mother Fucker")
 	}
 
 	for _, val := range ip {
