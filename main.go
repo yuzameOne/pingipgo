18194826f421bb94a28df2e221e8b6782a6f765d
yuzameOne <E.homayko@yandex.ru>
2020-11-18 22:27:45 +1000

    simple ping ip address

--- main.go
+++ main.go
@@ -6,7 +6,36 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"strings"
 )
+
+var ipLost string = "4.25.115.81"
+var ipReceived string = "87.250.250.242"
+
+func ping(ip ...string) {
+
+	// c - sending count ECHO_REQUEST packets
+	// i - Wait interval seconds between sending each packet
+	// Only super-user may set interval to values less than 0.2 seconds
+	// w - deadline Specify a timeout, in seconds, before ping exits regardless of how
+	// many packets have been sent or received.
+	fmt.Printf("ip array : %s", ip[1])
+	out, _ := exec.Command("ping", ipReceived, "-c 2", "-i 1", "-w 2").Output()
+
+	fmt.Println(string(out)) //dedug out
+
+	if strings.Contains(string(out), "100% packet loss") {
+		fmt.Println("Dead mother fucker")
+	} else {
+		fmt.Println("Yeap, I'am ALIVEEE")
+	}
+
+	for _, val := range ip {
+		fmt.Println(val)
+	}
+
+}
 
 func main() {
 
@@ -18,7 +47,7 @@ func main() {
 		log.Fatalf("failed opening file: %s", err)
 	}
 
-	//  bufio.NewScanner() передаем прочитанный файл
+	// bufio.NewScanner() передаем прочитанный файл
 	// bufio.ScanLines читает файл до спецсимволов "\n , \r"
 	// scanner.Split разделяет строки по спецсимволам "\n , \r"
 	scanner := bufio.NewScanner(file)
@@ -34,9 +63,11 @@ func main() {
 
 	file.Close()
 
-	for _, val := range iplines {
-		fmt.Println(val)
-	}
+	// for _, val := range iplines {
+	// 	fmt.Println(val)
+	// }
+
+	ping(iplines...)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
